96ff40d5fed9e4d7745ebf6c852a51ee6135c335
rafik021dev <rafaelsardinhadossantos101@gmail.com>
2025-10-02 10:15:02 -0300

    Update Registro.java

--- CursoAluno/src/Registro.java
+++ CursoAluno/src/Registro.java
@@ -1,2 +1,17 @@
+import java.util.Date;
+
 public class Registro {
+
+    private Date data;
+    private String tipo;
+    private Professor professor;
+
+    public Registro(Date data, String tipo, Professor professor) {
+        this.data = data;
+        this.tipo = tipo;
+        this.professor = professor;
+    }
+
+    
+
 }
