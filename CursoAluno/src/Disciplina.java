e91474bd615731b4e8193c8ad6bcb214e25f28ab
rafik021dev <rafaelsardinhadossantos101@gmail.com>
2025-10-02 10:14:04 -0300

    Update Disciplina.java

--- CursoAluno/src/Disciplina.java
+++ CursoAluno/src/Disciplina.java
@@ -1,5 +1,10 @@
-import java.util.Scanner;
-
 public class Disciplina {
 
+    private int codigo;
+    private String nome;
+    private boolean ativa;
+
+    // public Disciplina(int Codigo; String nome; boolean ativa) {
+
+    // }
 }
